2cec22f3c8033dbfa62d0e47ffd9ef6a1c548dd3
d0422 <rlfehd2013@naver.com>
2023-04-01 23:43:09 +0900

    [FEAT] Circle 애니메이션 추가

--- src/components/Interest.tsx
+++ src/components/Interest.tsx
@@ -1,21 +1,101 @@
+import { motion } from 'framer-motion';
 import styled from 'styled-components';
-
+const circleData = [
+  {
+    size: '185px',
+    x: '0px',
+    y: '0px',
+    color: '#FF7DA4',
+    text: '등산',
+  },
+  {
+    size: '100px',
+    x: '10px',
+    y: '185px',
+    color: '#FF8A80',
+    text: '활쏘기',
+  },
+  {
+    size: '54px',
+    x: '90px',
+    y: '262px',
+    color: '#B880FF',
+    text: '산책',
+  },
+  {
+    size: '100px',
+    x: '125px',
+    y: '180px',
+    color: '#8A80FF',
+    text: '말타기',
+  },
+  {
+    size: '60px',
+    x: '180px',
+    y: '120px',
+    color: '#FFEB80',
+    text: '요리',
+  },
+];
 const Interest = () => {
   return (
     <Wrapper>
       <Title>이순신님의 관심사</Title>
+      <CircleWrapper>
+        {circleData.map((data) => (
+          <Circle
+            size={data.size}
+            color={data.color}
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{
+              type: 'spring',
+              ease: 'easeOut',
+              duration: Math.random() * 2,
+            }}
+            x={data.x}
+            y={data.y}
+          >
+            {data.text}
+          </Circle>
+        ))}
+      </CircleWrapper>
     </Wrapper>
   );
 };
 
 export default Interest;
 const Wrapper = styled.div`
-  padding: 20px;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-bottom: 20px;
+  position: absolute;
 `;
 const Title = styled.div`
   font-size: 15px;
   font-weight: 700;
+  margin-bottom: 20px;
 `;
-const Circle = () => {
-  return <Circle></Circle>;
-};
+interface Props {
+  size: string;
+  color: string;
+  x: string;
+  y: string;
+}
+const CircleWrapper = styled.div`
+  position: relative;
+  height: 160px;
+`;
+const Circle = styled(motion.div)<Props>`
+  width: ${(props) => props.size};
+  height: ${(props) => props.size};
+  background-color: ${(props) => props.color};
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  position: absolute;
+  top: ${(props) => props.x};
+  left: ${(props) => props.y};
+`;
